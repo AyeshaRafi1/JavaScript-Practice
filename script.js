f7fa85f4d4b74f807d5d0f08ba1f6c4ced4d705d
Ayesha Rafi <ayesha.rafi@tintash.com>
2021-07-26 21:11:45 +0500

    implimented saving the todo list to the local storage and loaded the old todo list upon refresh

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 'use strict'
 
+// declaring all variables that are needed
+
 const addTaskBtn = document.querySelector('.btn');
 const valuee = document.querySelector('.input');
 const taskBox=document.querySelector('.tasks');
 
 
-let count=1;
-let taskNum=0;
-console.log(valuee.value)
+let Data="";
+let taskNum= localStorage.length ? Number(localStorage.getItem(localStorage.key(localStorage.length-1))[22]):0;
+console.log(localStorage.getItem(localStorage.key(localStorage.length))[22])
+console.log(localStorage.length)
+let numItems=0;
+
+//calculating length of non null values in local storage also creating the inner html code for the taskbox
+for (let i = 0; i < taskNum; i++) {
+    if (localStorage.getItem(i)!==null){
+        Data=Data+localStorage.getItem(i);
+        numItems=numItems+1
+    }        
+}
+
+
+// checking to see if we have something in local storage or not
+if (numItems===0){
+    taskBox.innerHTML="You currently do not have any tasks added";   
+}
+else{
+    taskBox.innerHTML="";
+    taskBox.innerHTML=Data;
+}
 
 
 // adding new tasks to the list on click of add task button
@@ -17,11 +39,11 @@ addTaskBtn.addEventListener('click',function(){
     const dateAndDay =`${dT.getMonth()+1}/${dT.getDate()}/${dT.getFullYear() } @ ${dT.getHours()}:${dT.getMinutes()}:${dT.getSeconds()}`
 
     //generating the required html
-    const html= `<div class="box" id="b${count}">
+    const html= `<div class="box" id="b${taskNum}">
                 <div class="box-upper">
                     <div class="box-upper-left">
                         <i class="left fa fa-tasks"></i>
-                        ToDo 0${count} ${valuee.value}
+                        ToDo 0${taskNum} ${valuee.value}
                     </div>
                     <div>
                         ${dateAndDay}
@@ -29,15 +51,16 @@ addTaskBtn.addEventListener('click',function(){
                 </div>
                 
                 <div class="box-lower">
-                    <i class="fa fa-pencil" id="p${count}"></i> 
-                    <i class="fa fa-check" id="c${count}"></i> 
-                    <i class="fa fa-remove" id="r${count}"></i>
+                    <i class="fa fa-pencil" id="p${taskNum}"></i> 
+                    <i class="fa fa-check" id="c${taskNum}"></i> 
+                    <i class="fa fa-remove" id="r${taskNum}"></i>
                 </div>
                  
             </div>`;
 
-    count=count+1;
-
+    localStorage.setItem(taskNum, html)
+            
+    console.log(taskNum)
     // adding the html at appropriate places
     if (taskNum===0){
         taskBox.innerHTML="";
@@ -47,7 +70,9 @@ addTaskBtn.addEventListener('click',function(){
     else{
         taskBox.innerHTML=taskBox.innerHTML+html;
         taskNum=taskNum+1
-}})
+}
+
+})
 
 // deleting a task
 taskBox.addEventListener('click', function (e){
@@ -55,11 +80,17 @@ taskBox.addEventListener('click', function (e){
         const idd="b"+e.target.id[1]
         const elem= document.getElementById(idd);
         elem.parentNode.removeChild(elem)
-        taskNum=taskNum-1
+        console.log(e.target.id[1])
+        localStorage.removeItem(e.target.id[1])
     }
     if (taskNum===0){
         taskBox.textContent="You currently do not have any tasks added";
     }
+    console.log("......................")
+    console.log(taskNum)
+    taskNum= Number(localStorage.getItem(localStorage.key(localStorage.length-1))[22])
+    console.log(taskNum)
+    
 
 })
 
